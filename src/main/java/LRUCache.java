d63ed94f5862f51ffe3f328e45ff5d8a10acce8d
Shuai Zhao <zhaoshua@amazon.co.jp>
2018-05-30 23:21:19 +0900

    Add log4j2 configuration and example
    log4j2 configuration example: https://howtodoinjava.com/log4j2/log4j-2-properties-file-configuration-example/

--- src/main/java/LRUCache.java
+++ src/main/java/LRUCache.java
@@ -1,23 +1,26 @@
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-class Node{
+class Node {
     int key;
     int value;
     Node previous;
     Node next;
 
-    Node(int key, int value){
+    Node(int key, int value) {
         this.key = key;
         this.value = value;
         this.previous = this;
         this.next = this;
     }
-    void delete(){
-        Node NodePrevious = this.previous;
-        Node NodeNext = this.next;
-        NodePrevious.next = NodeNext;
-        NodeNext.previous = NodePrevious;
+
+    void delete() {
+        Node nodePrevious = this.previous;
+        Node nodeNext = this.next;
+        nodePrevious.next = nodeNext;
+        nodeNext.previous = nodePrevious;
 
         this.previous = null;
         this.next = null;
@@ -25,78 +28,79 @@ class Node{
 }
 
 public class LRUCache {
-    Map<Integer, Node> map = new HashMap<>();
-    Node head = new Node(0,0);
-    int capacity;
-    int size;
+    private static final Logger log = LogManager.getLogger(LRUCache.class);
+    private Map<Integer, Node> map = new HashMap<>();
+    private Node head = new Node(0, 0);
+    private int capacity;
+    private int size;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
-
     }
 
     public int get(int key) {
         //check whether the node is present
         Node foundNode = map.get(key);
-        if (foundNode == null){
+        if (foundNode == null) {
             return -1;
-        }
-        else{
-            foundNode.delete();
-            insertNodeToTheEnd(foundNode);
+        } else {
+            moveNodeToEnd(foundNode);
 
             return foundNode.value;
         }
 
     }
-    private void insertNodeToTheEnd(Node newNode){
+
+    private void moveNodeToEnd(final Node foundNode) {
+        foundNode.delete();
+        insertNodeToTheEnd(foundNode);
+    }
+
+    private void insertNodeToTheEnd(Node node) {
         Node tail = head.previous;
-        tail.next = newNode;
-        newNode.previous = tail;
-        newNode.next = head;
-        head.previous = newNode;
+        tail.next = node;
+        node.previous = tail;
+        node.next = head;
+        head.previous = node;
     }
 
     public void put(int key, int value) {
         //check whether the node is present
         Node foundNode = map.get(key);
-        if (foundNode != null){
+        if (foundNode != null) {
             foundNode.value = value;
-            map.remove(key);
-            map.put(key, foundNode);
-            this.get(key);
-        }
-        else{
+            moveNodeToEnd(foundNode);
+        } else {
             //insert the new node
             Node newNode = new Node(key, value);
             map.put(key, newNode);
             insertNodeToTheEnd(newNode);
             size++;
 
-            if (size>capacity){
+            if (size > capacity) {
                 Node toDeleteNode = head.next;
                 map.remove(toDeleteNode.key);
                 toDeleteNode.delete();
                 size--;
-
             }
+            log.info("current size is {}, capacity is {}", size, capacity);
 
         }
 
 
-
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         LRUCache obj = new LRUCache(3);
-        obj.put(1,1);
-        obj.put(2,2);
-        obj.put(3,3);
-        obj.put(4,4);
+        obj.put(1, 1);
+        obj.put(2, 2);
+        obj.put(3, 3);
+        obj.put(4, 4);
         System.out.println(obj.get(4));
         System.out.println(obj.get(3));
         System.out.println(obj.get(2));
         System.out.println(obj.get(1));
-        obj.put(5,5);
+        obj.put(5, 5);
         //obj.put(4,1);
         System.out.println(obj.get(1));
         // obj.put(4,4);

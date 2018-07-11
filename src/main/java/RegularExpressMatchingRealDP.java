598061f0ef75177eb3ff3cd46de1931988298fe7
admin <chensi910@gmail.com>
2018-07-11 16:36:44 +0900

    RegularExpressMatchRealDP rewrite

--- src/main/java/RegularExpressMatchingRealDP.java
+++ src/main/java/RegularExpressMatchingRealDP.java
@@ -5,7 +5,6 @@ public class RegularExpressMatchingRealDP {
     // -1 initial
     // 0 not match
     // 1 match
-    // 2 ignore
 
     public boolean isMatch(String s, String p) {
         this.s = s;
@@ -23,12 +22,9 @@ public class RegularExpressMatchingRealDP {
     }
 
     private boolean match(int pointerForS, int pointerForP) {
-        if (state[pointerForS + 1][pointerForP + 1] == 1) {
-            return true;
-        }
 
-        if (state[pointerForS + 1][pointerForP + 1] == 0) {
-            return false;
+        if ( state[pointerForS + 1][pointerForP + 1] != -1 ) {
+            return state[pointerForS + 1][pointerForP + 1] == 1 ? true:false;
         }
 
         //S is blank while P is not blank
@@ -69,7 +65,7 @@ public class RegularExpressMatchingRealDP {
                 return false;
             }
         }else{
-            if (pointerForP - 1 >= 0  && p.charAt(pointerForP) == '*' && p.charAt(pointerForP - 1) != s.charAt(pointerForS) && p.charAt(pointerForP - 1) != '.') {
+            if ( p.charAt(pointerForP) == '*' && p.charAt(pointerForP - 1) != s.charAt(pointerForS) && p.charAt(pointerForP - 1) != '.') {
                 if ( match(pointerForS, pointerForP - 2) ) {
                     state[pointerForS + 1][pointerForP + 1] = 1;
                     state[pointerForS + 1][pointerForP] = 0;
@@ -82,34 +78,20 @@ public class RegularExpressMatchingRealDP {
                 }
 
             }else {
-                if ( pointerForS>=0 && p.charAt(pointerForP) == '*' && (p.charAt(pointerForP - 1) == '.'|| s.charAt(pointerForS) == p.charAt(pointerForP - 1) )) {
-                    int count = 0;
-                    while ( pointerForS >= 0 && (p.charAt(pointerForP - 1) == '.'|| s.charAt(pointerForS) == p.charAt(pointerForP - 1)) ) {
-
-                        if ( match(pointerForS, pointerForP - 2 ) ){
-
-                            state[pointerForS + count + 1][pointerForP + 1 ] = 1;
-                            state[pointerForS + count + 1][pointerForP] = 0;
+                if ( p.charAt(pointerForP) == '*' && (p.charAt(pointerForP - 1) == '.'|| s.charAt(pointerForS) == p.charAt(pointerForP - 1) )) {
+                    if ( match(pointerForS, pointerForP - 2) ) {
+                        state[pointerForS + 1][pointerForP + 1] = 1;
+                        return true;
+                    } else{
+                        if ( match(pointerForS-1, pointerForP) ) {
+                            state[pointerForS + 1][pointerForP + 1] = 1;
                             return true;
                         }else {
-                            state[pointerForS + count + 1][pointerForP + 1 ] = 0;
-                            state[pointerForS + count + 1][pointerForP] = 0;
-                            count++;
-                            pointerForS--;
+                            state[pointerForS + 1][pointerForP + 1] = 0;
+                            return false;
                         }
-                    }
-                    pointerForP = pointerForP - 2;
-                    if ( match(pointerForS, pointerForP)) {
-                        state[pointerForS + count + 1][pointerForP + 2 + 1 ] = 1;
-                        state[pointerForS + count + 1][pointerForP + 2 ] = 0;
-                        return true;
-                    }
-                    else {
-                        state[pointerForS + count + 1][pointerForP + 2 + 1 ] = 0;
-                        state[pointerForS + count + 1][pointerForP + 2 ] = 0;
-                        return false;
-                    }
 
+                    }
                 }
                 else{
                     state[pointerForS + 1][pointerForP + 1] = 0;
